050e561a40bb4cd4263b46474ce2a931ba04f020
nadia <nadiakhalifa1606@gmail.com>
2024-09-19 22:42:22 +0100

    delete post

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-app.post('/add-user', (req, res) => {
-  res.send('user added')
-})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
